155e88fbbbe4ce82ae80771b1ca62a46beae884c
Mohamed Hamisa <108889205+MohamedHamisa@users.noreply.github.com>
2023-02-04 23:26:57 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int):
+        l, ans, product = 0, 0, 1
+        for r in range(len(nums)):
+            product *= nums[r]
+            if product < k:
+                ans += r-l+1
+            else:
+                while l <= r and product >= k:
+                    product = product / nums[l]
+                    l += 1
+                ans += r-l+1
+        return ans
+      
+      
